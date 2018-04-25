ab1eb558341d3303aaa78d3754ccf699958a1d7a
Wojciech Jasiński <ilikeheaps@gmail.com>
2018-04-25 19:34:09 +0200

    Prepare some stuff for refactoring

--- src/MonadStuff.hs
+++ src/MonadStuff.hs
@@ -3,10 +3,16 @@ import Data.Functor.Identity
 import Control.Monad
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.Maybe
+import Control.Monad.Trans.Except
 import Control.Monad.Trans.Reader
+
 
 liftMaybe :: (MonadPlus m) => Maybe a -> m a
 liftMaybe = maybe mzero return
+
+maybeFail :: Monad m => e -> Maybe a -> ExceptT e m a
+maybeFail _e (Just x) = pure x
+maybeFail e Nothing = throwE e
 
 readerT :: Monad m => Reader a b -> ReaderT a m b
 readerT = mapReaderT (return . runIdentity)
