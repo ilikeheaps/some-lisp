1f6efbd40098305f8c46e4e77763ac877cf3c7e8
Wojciech Jasiński <ilikeheaps@gmail.com>
2018-07-23 01:27:17 +0200

    Add some TODO

--- src/Lisp.hs
+++ src/Lisp.hs
@@ -11,6 +11,7 @@ defaultEnv = bindPureFun "+" (\(EInt a :.: EInt b :.: ENil) ->  EInt $ a + b)
              . bindPureFun "*" (\(EInt a :.: EInt b :.: ENil) ->  EInt $ a * b)
              . bindPureFun "-" (\(EInt a :.: EInt b :.: ENil) ->  EInt $ a - b)
              . bindPureFun "div" (\(EInt a :.: EInt b :.: ENil) ->  EInt $ a `div` b)
+             -- TODO this =if= is strict!
              . bindPureFun "if" (\(EBool p :.: v1 :.: v2 :.: ENil) ->  if p then v1 else v2)
              . bindPureFun "<" (\(EInt a :.: EInt b :.: ENil) ->  EBool $ a < b)
              . bindPureFun "=" (\(EInt a :.: EInt b :.: ENil) ->  EBool $ a == b)
--- src/Lisp/Environment.hs
+++ src/Lisp/Environment.hs
@@ -10,6 +10,7 @@ bindName s = bindVar s
 bindFun :: String -> (Expr -> EvalM Expr) -> Env -> Env
 bindFun name body = bindName name (EHaskellFun body)
 
+-- TODO rename to bindStrictFun?
 -- the bound function will be strict
 bindPureFun :: String -> (Expr -> Expr) -> Env -> Env
 bindPureFun name fun = bindName name (EHaskellFun action)
