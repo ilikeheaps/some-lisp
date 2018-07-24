764bb67892c8e6ad057a157bf518e8ececa93711
Wojciech Jasiński <ilikeheaps@gmail.com>
2018-07-25 00:41:48 +0200

    Make `div` gracefully fail

--- src/Lisp.hs
+++ src/Lisp.hs
@@ -1,6 +1,7 @@
 import Control.Monad.Trans.Reader
 import Control.Monad.Trans.Maybe
 import System.IO.Unsafe
+import Control.Monad (when)
 
 import Lisp.Types
 import Lisp.Environment
@@ -10,7 +11,12 @@ defaultEnv :: Env
 defaultEnv = bindPureFun "+" (\(EInt a :.: EInt b :.: ENil) ->  EInt $ a + b)
              . bindPureFun "*" (\(EInt a :.: EInt b :.: ENil) ->  EInt $ a * b)
              . bindPureFun "-" (\(EInt a :.: EInt b :.: ENil) ->  EInt $ a - b)
-             . bindPureFun "div" (\(EInt a :.: EInt b :.: ENil) ->  EInt $ a `div` b)
+             . bindExceptFun "div"
+               (\expr -> case expr of
+                           (EInt a :.: EInt b :.: ENil) -> do
+                             when (b == 0) $ lispError "div by 0"
+                             pure $ EInt $ a `div` b
+                           _ -> lispError "Wrong arguments for div")
              -- TODO this =if= is strict!
              . bindPureFun "if" (\(EBool p :.: v1 :.: v2 :.: ENil) ->  if p then v1 else v2)
              . bindPureFun "<" (\(EInt a :.: EInt b :.: ENil) ->  EBool $ a < b)
