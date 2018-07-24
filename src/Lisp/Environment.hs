ed0ae47c1f47721de11eb83b8778bce616dfb5fd
Wojciech Jasiński <ilikeheaps@gmail.com>
2018-07-25 00:40:23 +0200

    Introduce binding functions (actions) that can fail

--- src/Lisp/Environment.hs
+++ src/Lisp/Environment.hs
@@ -1,5 +1,10 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE Rank2Types #-}
 module Lisp.Environment where
 
+import Control.Monad.Trans.Except
+
+import MonadStuff
 import Lisp.Eval
 import Lisp.Environment.Basic
 import Lisp.Types
@@ -15,6 +20,19 @@ bindPureFun name fun = bindName name (EHaskellFun action)
   where action args = do
           values <- evalList args
           pure $ fun values
+
+bindFailFun :: String -> String -> (Expr -> Maybe Expr) -> Env -> Env
+bindFailFun name errMsg fun = bindName name (EHaskellFun action)
+  where action args = do
+          values <- evalList args
+          maybeFail (EvalExc errMsg) $ fun values
+
+bindExceptFun :: String -> (forall m . Monad m => Expr -> ExceptT EvalError m Expr) -> Env -> Env
+bindExceptFun name fun = bindName name (EHaskellFun action)
+  where action :: Expr -> EvalM Expr
+        action args = do
+          values <- evalList args
+          fun values
 
 bindPureForm :: String -> (Expr -> Expr) -> Env -> Env
 bindPureForm name fun = bindName name (EHaskellFun (pure . fun))
