9e2d77d881056d08f9bc9f49ddbe8603ee4da8cd
Wojciech Jasiński <ilikeheaps@gmail.com>
2018-07-25 02:10:49 +0200

    Use lambda case somewhere

--- src/Lisp.hs
+++ src/Lisp.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 import Control.Monad.Trans.Reader
 import Control.Monad (when)
 
@@ -10,11 +11,10 @@ defaultEnv = bindPureFun "+" (\(EInt a :.: EInt b :.: ENil) ->  EInt $ a + b)
              . bindPureFun "*" (\(EInt a :.: EInt b :.: ENil) ->  EInt $ a * b)
              . bindPureFun "-" (\(EInt a :.: EInt b :.: ENil) ->  EInt $ a - b)
              . bindExceptFun "div"
-               (\expr -> case expr of
-                           (EInt a :.: EInt b :.: ENil) -> do
-                             when (b == 0) $ lispError "div by 0"
-                             pure $ EInt $ a `div` b
-                           _ -> lispError "Wrong arguments for div")
+               (\case (EInt a :.: EInt b :.: ENil) -> do
+                        when (b == 0) $ lispError "div by 0"
+                        pure $ EInt $ a `div` b
+                      _ -> lispError "Wrong arguments for div")
              -- TODO this =if= is strict!
              . bindPureFun "if" (\(EBool p :.: v1 :.: v2 :.: ENil) ->  if p then v1 else v2)
              . bindPureFun "<" (\(EInt a :.: EInt b :.: ENil) ->  EBool $ a < b)
